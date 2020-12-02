b7c5a861529a2ecd7af0dbef67048d81e16cdea3
henkla <lthlarsson@gmail.com>
2020-12-02 12:09:06 +0100

    Preparing even more!

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -11,18 +11,24 @@ namespace Day03
             var inputHelper = new InputHelper();
             var input = inputHelper.GetInput("input.txt");
 
+            // operate on input?
+
             Part1(input);
             Part2(input);
-        }
-
-        private static void Part2(IEnumerable<string> input)
-        {
-            throw new NotImplementedException();
         }
 
         private static void Part1(IEnumerable<string> input)
         {
             throw new NotImplementedException();
+            Console.WriteLine("Part 1:\n");
+            Console.ReadKey();
+        }
+
+        private static void Part2(IEnumerable<string> input)
+        {
+            throw new NotImplementedException();
+            Console.WriteLine("Part 2:\n");
+            Console.ReadKey();
         }
     }
 }
