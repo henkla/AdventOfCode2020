d0403603137e96a934b32055679d58c2bdc8a2b2
henkla <lthlarsson@gmail.com>
2020-12-04 23:31:05 +0100

    Preparing for the next day's challenge

---
+++ Day05/Program.cs
@@ -0,0 +1,43 @@
+﻿using AdventOfCode2020.Library;
+using System;
+using System.Collections.Generic;
+
+namespace Day05
+{
+    class Program
+    {
+        /*
+        <description>
+         */
+        static void Main(string[] args)
+        {
+            var inputHelper = new InputHelper();
+            var input = inputHelper.GetInputAsLines("input.txt");
+
+            // operate on input?
+
+            Part1(input);
+            Part2(input);
+        }
+
+        /*
+        <description>
+         */
+        private static void Part1(IEnumerable<string> input)
+        {
+            throw new NotImplementedException();
+            Console.WriteLine("\nPart 1:\n");
+            Console.ReadKey();
+        }
+
+        /*
+        <description>
+         */
+        private static void Part2(IEnumerable<string> input)
+        {
+            throw new NotImplementedException();
+            Console.WriteLine("\nPart 2:\n");
+            Console.ReadKey();
+        }
+    }
+}
