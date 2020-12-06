1cf8c09620f192a1504e55b0f2e69728bbb028ed
henkla <lthlarsson@gmail.com>
2020-12-06 08:37:03 +0100

    Part A finished

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,40 +1,145 @@
 ﻿using AdventOfCode2020.Library;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Day06
 {
     class Program
     {
         /*
-        <description> 
+        --- Day 6: Custom Customs ---
+        As your flight approaches the regional airport where you'll switch 
+        to a much larger plane, customs declaration forms are distributed 
+        to the passengers.
+        
+        The form asks a series of 26 yes-or-no questions marked a through z. 
+        All you need to do is identify the questions for which anyone in 
+        your group answers "yes". Since your group is just you, this doesn't 
+        take very long.
+        
+        However, the person sitting next to you seems to be experiencing a 
+        language barrier and asks if you can help. For each of the people 
+        in their group, you write down the questions for which they answer 
+        "yes", one per line. For example:
+        
+        abcx
+        abcy
+        abcz
+        In this group, there are 6 questions to which anyone answered "yes": 
+        a, b, c, x, y, and z. (Duplicate answers to the same question don't 
+        count extra; each question counts at most once.) 
          */
         static void Main(string[] args)
         {
             var inputHelper = new InputHelper();
-            var input = inputHelper.GetInputAsLines("example.txt");
+            var input = inputHelper.GetInputAsLines("input.txt");
 
             // operate on input?
 
             Part1(input);
-            Part2(input);
+            //Part2(input);
         }
 
         /*
-        <description> 
+        Another group asks for your help, then another, and eventually you've collected 
+        answers from every group on the plane (your puzzle input). Each group's answers 
+        are separated by a blank line, and within each group, each person's answers are 
+        on a single line. For example:
+
+        <example.txt>
+
+        This list represents answers from five groups:
+        
+        The first group contains one person who answered "yes" to 3 questions: a, b, and 
+        c. The second group contains three people; combined, they answered "yes" to 3 
+        questions: a, b, and c. The third group contains two people; combined, they 
+        answered "yes" to 3 questions: a, b, and c. The fourth group contains four people; 
+        combined, they answered "yes" to only 1 question, a. The last group contains one 
+        person who answered "yes" to only 1 question, b.
+        In this example, the sum of these counts is 3 + 3 + 3 + 1 + 1 = 11.
+        
+        For each group, count the number of questions to which anyone answered "yes". 
+        What is the sum of those counts?
          */
         private static void Part1(IEnumerable<string> input)
         {
-            throw new NotImplementedException();
             Console.WriteLine("\nPart 1:\n");
 
-            // do stuff
+            // parse input
+            var groupAnswers = ParseAllAnswers(input);
 
+            // for part 1 - run distinct and sum all groups together
+            var sum = 0;
+            foreach (var answer in groupAnswers)
+            {
+                sum += answer.Distinct().Count();
+            }
+
+            Console.WriteLine($"For part 1, the resulting sum is {sum}.");
             Console.ReadKey();
         }
 
+        private static IEnumerable<string> ParseAllAnswers(IEnumerable<string> input)
+        {
+            var groupAnswers = new List<string>();
+            var currentGroup = string.Empty;
+
+            foreach (var line in input)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    groupAnswers.Add(currentGroup);
+                    currentGroup = string.Empty;
+                }
+                else
+                {
+                    currentGroup += line;
+                }
+            }
+
+            groupAnswers.Add(currentGroup);
+            return groupAnswers;
+        }
+
         /*
-        <description> 
+        As you finish the last group's customs declaration, you 
+        notice that you misread one word in the instructions:
+
+        You don't need to identify the questions to which anyone 
+        answered "yes"; you need to identify the questions to which 
+        everyone answered "yes"!
+        
+        Using the same example as above:
+        
+        abc
+        
+        a
+        b
+        c
+        
+        ab
+        ac
+        
+        a
+        a
+        a
+        a
+        
+        b
+        This list represents answers from five groups:
+        
+        In the first group, everyone (all 1 person) answered "yes" to 3 questions: a, 
+            b, and c.
+        In the second group, there is no question to which everyone answered "yes".
+        In the third group, everyone answered yes to only 1 question, a. Since some 
+            people did not answer "yes" to b or c, they don't count.
+        In the fourth group, everyone answered yes to only 1 question, a.
+        In the fifth group, everyone (all 1 person) answered "yes" to 1 question, b.
+        In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+        
+        For each group, count the number of questions to which everyone answered "yes". 
+        What is the sum of those counts?
          */
         private static void Part2(IEnumerable<string> input)
         {
