83c7550c2a9d019530d018b68ebbc34ba9c31da7
henkla <lthlarsson@gmail.com>
2020-12-07 23:36:49 +0100

    Preparing for next day

---
+++ Day08/Program.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCode2020.Library;
+using System;
+using System.Collections.Generic;
+
+namespace Day08
+{
+    class Program
+    {
+        /*
+         <description>
+         */
+        static void Main(string[] args)
+        {
+            var inputHelper = new InputHelper();
+            var input = inputHelper.GetInputAsLines("example.txt");
+
+            Part1(input);
+            Part2(input);
+        }
+
+        /*
+         <description>
+         */
+        private static void Part1(IEnumerable<string> input)
+        {
+            throw new NotImplementedException();
+            Console.WriteLine("\nPart 1:\n");
+
+            // do stuff
+
+            Console.WriteLine($"For part 1, the result is RESULT.");
+            Console.ReadKey();
+        }
+
+        /*
+         <description>
+         */
+        private static void Part2(IEnumerable<string> input)
+        {
+            throw new NotImplementedException();
+            Console.WriteLine("\nPart 2:\n");
+
+            // do stuff
+
+            Console.WriteLine($"For part 2, the result is RESULT.");
+            Console.ReadKey();
+        }
+    }
+}
