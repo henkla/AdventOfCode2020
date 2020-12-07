7ea65c587647fcdf5f93061f5b1ff5d1f3d3db60
henkla <lthlarsson@gmail.com>
2020-12-07 21:30:42 +0100

    Part A done

---
+++ Day07/Rule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day07
+{
+    public class Rule
+    {
+        public string Type { get; private set; }
+        public ICollection<Rule> Parents { get; set; } = new List<Rule>();
+        public ICollection<Rule> Children { get; set; } = new List<Rule>();
+        public string Contents;
+
+        public Rule(string type, string contents)
+        {
+            Type = type;
+            Contents = contents;
+        }
+    }
+}
