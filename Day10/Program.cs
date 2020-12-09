76774fe990153a5e93765e110abd01e068345c9c
henkla <lthlarsson@gmail.com>
2020-12-09 10:26:37 +0100

    Preparing for next day's challenge

---
+++ Day10/Program.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode2020.Library;
+using System;
+using System.Collections.Generic;
+
+namespace Day10
+{
+    class Program
+    {
+        /*
+        <description>
+         */
+        static void Main(string[] args)
+        {
+            var inputHelper = new InputHelper();
+            var input = inputHelper.GetInputAsLines("input.txt");
+
+            // parse?
+
+            var resultPart1 = Part1(input);
+            Console.WriteLine($"For Part 1, the result is {resultPart1}");
+            Console.ReadKey();
+
+            var resultPart2 = Part2(input);
+            Console.WriteLine($"For Part 2, the result is {resultPart2}");
+            Console.ReadKey();
+        }
+
+        /*
+        <description>
+         */
+        private static int Part1(IEnumerable<string> input)
+        {
+            throw new NotImplementedException();
+            Console.WriteLine("\nPart 1: \n");
+
+            // do stuff
+            var result = 0;
+
+            return result;
+        }
+
+        /*
+        <description>
+         */
+        private static int Part2(IEnumerable<string> input)
+        {
+            throw new NotImplementedException();
+            Console.WriteLine("\nPart 2: \n");
+
+            // do stuff
+            var result = 0;
+
+            return result;
+        }
+    }
+}
