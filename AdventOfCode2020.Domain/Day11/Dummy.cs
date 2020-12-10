8c5557b7cee24fe410c4277f241029a0d0a17b71
henkla <lthlarsson@gmail.com>
2020-12-10 22:46:07 +0100

    Preparing for next day's challenge

---
+++ AdventOfCode2020.Domain/Day11/Dummy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2020.Domain.Day11
+{
+    public class Dummy : BaseChallenge
+    {
+        private IEnumerable<string> _input;
+
+        protected override void Initialize()
+        {
+            _input = InputReader.ReadFile("example");
+        }
+
+        protected override void SolveFirst()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void SolveSecond()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
