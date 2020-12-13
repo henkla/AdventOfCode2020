89d0bdbcb9a9afa4084253898b3d838faa22f258
henkla <lthlarsson@gmail.com>
2020-12-13 09:28:54 +0100

    Part A finished

--- AdventOfCode2020.Domain/Day13/ShuttleSearch.cs
+++ AdventOfCode2020.Domain/Day13/ShuttleSearch.cs
@@ -1,19 +1,37 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2020.Domain.Day13
 {
     public class ShuttleSearch : BaseChallenge
     {
-        private IEnumerable<string> _input;
+        private string[] _input;
 
         protected override void Initialize()
         {
-            _input = InputReader.ReadFile("example.txt");
+            _input = InputReader.ReadFile("input.txt").ToArray();
         }
 
         protected override void SolveFirst()
         {
-            throw new System.NotImplementedException();
+            var timestamp = int.Parse(_input[0]);
+            var ids = _input[1].Split(',');
+            var table = new Dictionary<int, int>();
+
+            foreach (var id in ids.Where(s => !string.Equals(s, "x")).Select(s => int.Parse(s)).ToList())
+            {
+                var value = 0;
+                while (value < timestamp)
+                {
+                    value += id;
+                }
+
+                table.Add(id, value);
+            }
+
+            var target = table.Where(kv => kv.Value > timestamp).OrderBy(kv => kv.Value).First();
+            Result.First = (target.Value - timestamp) * target.Key;
+
         }
 
         protected override void SolveSecond()
