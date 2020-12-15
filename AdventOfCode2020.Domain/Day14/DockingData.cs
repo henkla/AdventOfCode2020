593a51f1600c99191c704ddf698b1ec323588de9
henkla <lthlarsson@gmail.com>
2020-12-15 08:13:27 +0100

    Debugging 14B, which I feel should work (but it does not)

--- AdventOfCode2020.Domain/Day14/DockingData.cs
+++ AdventOfCode2020.Domain/Day14/DockingData.cs
@@ -12,22 +12,13 @@ namespace AdventOfCode2020.Domain.Day14
 
         protected override void Initialize()
         {
-            _input = InputReader.ReadFile("example.txt");
+            _input = InputReader.ReadFile("input.txt");
         }
 
         protected override void SolveFirst()
         {
-            var memory = new Dictionary<string, string>();
+            var register = new Dictionary<int, string>();
             var mask = string.Empty;
-
-            void AddToMemory(string mem, string value)
-            {
-                var key = Regex.Match(mem, @"\d+").Value;
-                if (memory.ContainsKey(key))
-                    memory[key] = value;
-                else
-                    memory.Add(key, value);
-            }
 
             foreach (var line in _input)
             {
@@ -39,24 +30,69 @@ namespace AdventOfCode2020.Domain.Day14
                         mask = RHS;
                         break;
                     default:
-                        AddToMemory(LHS, ApplyMask(RHS, mask));
+                        var binary = ApplyMask(int.Parse(RHS), mask);
+                        var key = ExtractDigitsFromString(LHS);
+                        AddToRegister(register, key, binary);
                         break;
                 }
             }
 
-            memory.Select(kv => kv.Value).ToList().ForEach(v => Result.First += Convert.ToInt64(v, 2));
+            register.Select(kv => kv.Value).ToList().ForEach(v => Result.First += Convert.ToInt64(v, 2));
+        
         }
-        private string ApplyMask(string mem, string mask)
+
+        private int ExtractDigitsFromString(string source)
+        {
+            return int.Parse(Regex.Match(source, @"\d+").Value);
+        }
+
+        private void AddToRegister(IDictionary<int, string> register, int key, string value)
+        {
+            if (register.ContainsKey(key))
+                register[key] = value;
+            else
+                register.Add(key, value);
+        }
+
+        private string ConvertIntToBinary(int source, int length) 
+        {
+            return Convert.ToString(source, 2).PadLeft(length, '0');
+        }
+
+        private string ApplyMask(int value, string mask)
         {
             var result = Enumerable.Repeat('0', mask.Length).ToArray();
-            mem = $"{int.Parse(mem):D36}";
-            
-            for (int index = mem.Length - 1; index >= 0; index--)
+            var binary = ConvertIntToBinary(value, 36);
+
+            for (int index = binary.Length - 1; index >= 0; index--)
             {
                 if (Equals(mask[index], 'X'))
                 {
-                    result[index] = mem[index];
+                    result[index] = binary[index];
                 }
+                else
+                {
+                    result[index] = mask[index];
+                }
+            }
+
+            return new string(result);
+        }
+
+        private string ApplyMask2(int value, string mask)
+        {
+            var result = Enumerable.Repeat('0', mask.Length).ToArray();
+            var binary = ConvertIntToBinary(value, 36);
+            
+            for (int index = binary.Length - 1; index >= 0; index--)
+            {
+                // If the bitmask bit is 0, the corresponding memory address bit is unchanged
+                if (Equals(mask[index], '0'))
+                {
+                    result[index] = binary[index];
+                }
+                // If the bitmask bit is 1, the corresponding memory address bit is overwritten with 1
+                // If the bitmask bit is X, the corresponding memory address bit is floating.
                 else
                 {
                     result[index] = mask[index];
@@ -74,7 +110,56 @@ namespace AdventOfCode2020.Domain.Day14
 
         protected override void SolveSecond()
         {
-            
+            var register = new Dictionary<string, string>();
+            var mask = string.Empty;
+
+            foreach (var line in _input)
+            {
+                var (LHS, RHS) = ParseInstruction(line);
+
+                switch (LHS)
+                {
+                    case "mask":
+                        mask = RHS;
+                        break;
+                    default:
+                        var key = ApplyMask2(ExtractDigitsFromString(LHS), mask);
+                        AddToRegister2(register, key, RHS);
+                        break;
+                }
+            }
+
+            register.Select(kv => kv.Value).ToList().ForEach(v => Result.Second += int.Parse(v));
+        }
+
+        private void AddToRegister2(Dictionary<string, string> register, string key, string value)
+        {
+            var keys = new List<string>() { key };
+            while (keys.Any(s => s.Contains('X')))
+            {
+                var xKey = keys.First(s => s.Contains('X'));
+                foreach (var index in Enumerable.Range(0, key.Length))
+                {
+                    if (xKey[index] == 'X')
+                    {
+                        var newKey = new StringBuilder(xKey);
+                        newKey[index] = '0';
+                        keys.Add(newKey.ToString());
+                        newKey[index] = '1';
+                        keys.Add(newKey.ToString());
+                        keys.Remove(xKey);
+                        break;
+                    }
+                }
+            }
+
+            foreach (var k in keys)
+            {
+                if (register.ContainsKey(k))
+                    register[key] = value;
+                else
+                    register.Add(k, value);
+            }
         }
     }
 }
